3f1c758602c1d535bb772191b1156e2bc19aad95
YifePlayte <yife-playte@foxmail.com>
2021-11-30 01:01:58 +0800

    update

---
+++ device.mk
@@ -0,0 +1,40 @@
+LOCAL_PATH := device/xiaomi/elish
+
+# API
+PRODUCT_SHIPPING_API_LEVEL := 30
+
+# Dynamic
+PRODUCT_USE_DYNAMIC_PARTITIONS := true
+
+# Enable project quotas and casefolding for emulated storage without sdcardfs
+$(call inherit-product, $(SRC_TARGET_DIR)/product/emulated_storage.mk)
+# A/B
+ENABLE_VIRTUAL_AB := true
+$(call inherit-product, $(SRC_TARGET_DIR)/product/virtual_ab_ota.mk)
+
+# A/B
+AB_OTA_UPDATER := true
+AB_OTA_PARTITIONS += \
+    boot \
+    system \
+    product \
+    dtbo \
+    vbmeta \
+    vbmeta_system \
+    vendor
+
+PRODUCT_PACKAGES += \
+    otapreopt_script \
+    update_engine \
+    update_engine_sideload \
+    update_verifier
+
+AB_OTA_POSTINSTALL_CONFIG += \
+    RUN_POSTINSTALL_system=true \
+    POSTINSTALL_PATH_system=system/bin/otapreopt_script \
+    FILESYSTEM_TYPE_system=ext4 \
+    POSTINSTALL_OPTIONAL_system=true
+
+# Boot control HAL
+PRODUCT_PACKAGES += \
+    android.hardware.boot@1.1-impl.recovery
---
+++ twrp_elish.mk
@@ -0,0 +1,20 @@
+# Inherit from those products. Most specific first.
+$(call inherit-product, $(SRC_TARET_DIR)/product/core_64_bit.mk)
+$(call inherit-product, $(SRC_TARET_DIR)/product/base.mk)
+
+# Install gsi keys into ramdisk, to boot a developer GSI with verified boot.
+$(call inherit-product, $(SRC_TARET_DIR)/product/gsi_keys.mk)
+
+# Inherit from our custom product configuration
+$(call inherit-product, vendor/twrp/config/common.mk)
+
+# Device specific configs
+$(call inherit-product, device/xiaomi/elish/device.mk)
+
+# Device identifier. This must come after all inclusions
+PRODUCT_DEVICE := elish
+PRODUCT_NAME := twrp_elish
+PRODUCT_BRAND := xiaomi
+PRODUCT_MODEL := xiaomi
+PRODUCT_MANUFACTURER := xiaomi
+PRODUCT_RELEASE_NAME := xiaomi
