04df2327644b5cf9306e80967691751d0a0a1a62
YifePlayte <yife-playte@foxmail.com>
2021-11-30 14:26:04 +0800

    update

--- twrp_elish.mk
+++ twrp_elish.mk
@@ -1,9 +1,9 @@
 # Inherit from those products. Most specific first.
-$(call inherit-product, $(SRC_TARET_DIR)/product/core_64_bit.mk)
-#$(call inherit-product, $(SRC_TARET_DIR)/product/base.mk)
+$(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
+$(call inherit-product, $(SRC_TARGET_DIR)/product/base.mk)
 
 # Install gsi keys into ramdisk, to boot a developer GSI with verified boot.
-$(call inherit-product, $(SRC_TARET_DIR)/product/gsi_keys.mk)
+$(call inherit-product, $(SRC_TARGET_DIR)/product/gsi_keys.mk)
 
 # Inherit from our custom product configuration
 $(call inherit-product, vendor/twrp/config/common.mk)
